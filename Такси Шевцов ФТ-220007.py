62d48720954203252b44e23fca064ff7a1cb2c47
mnxactep <MNXACTEP2@gmail.com>
2023-11-07 19:35:05 +0500

    Добавил проверку правильности введённых данных

--- Такси Шевцов ФТ-220007.py
+++ Такси Шевцов ФТ-220007.py
@@ -5,16 +5,6 @@ def taxi_sort(N, distances, tariffs):
         total_cost += distances[i] * tariffs[sorted_taxis[i]]
     return sorted_taxis, total_cost
 
-N = int(input("Введите количество сотрудников компании: "))
-distances = list(map(int, input("Введите расстояния от работы до домов: ").split()))
-tariffs = list(map(int, input("Введите тарифы за проезд одного километра в такси: ").split()))
-
-sorted_taxis, total_cost = taxi_sort(N, distances, tariffs)
-
-print("Номера такси для сотрудников:", sorted_taxis)
-print("Сумма в рублях:", total_cost)
-
-# Функция для преобразования числа в слово
 def num2word(n, words):
     if n % 100 in (11, 12, 13, 14):
         return words[2]
@@ -24,6 +14,32 @@ def num2word(n, words):
         return words[1]
     return words[2]
 
-# Вывод суммы прописью
-rubles_words = ["рубль", "рубля", "рублей"]
-print("Сумма словами:", total_cost, num2word(total_cost, rubles_words))
+def main():
+    try:
+        N = int(input("Введите количество сотрудников компании: "))
+        if N <= 0:
+            print("Количество сотрудников должно быть больше нуля")
+            return
+
+        distances = list(map(int, input("Введите расстояния от работы до домов: ").split()))
+        if len(distances) != N:
+            print("Количество расстояний должно соответствовать количеству сотрудников")
+            return
+
+        tariffs = list(map(int, input("Введите тарифы за проезд одного километра в такси: ").split()))
+        if len(tariffs) != N:
+            print("Количество тарифов должно соответствовать количеству сотрудников")
+            return
+
+        sorted_taxis, total_cost = taxi_sort(N, distances, tariffs)
+
+        print("Номера такси для сотрудников:", sorted_taxis)
+        print("Сумма в рублях:", total_cost)
+
+        rubles_words = ["рубль", "рубля", "рублей"]
+        print("Сумма словами:", total_cost, num2word(total_cost, rubles_words))
+    except ValueError:
+        print("Ошибка ввода. Введите целые числа.")
+
+if __name__ == "__main__":
+    main()
